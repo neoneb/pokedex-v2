126f4c5bbcf9ba7f176724ccb12995a1001de5d7
neoneb <87101137+neoneb@users.noreply.github.com>
2023-05-06 15:07:13 -0700

    initial boiler plate and component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,10 @@
+function SearchBar() {
+  return (
+    <div>
+      <input placeholder='Search a Pokemon...' className='input' />
+      <button>Search</button>
+    </div>
+  );
+}
+
+export default SearchBar;
---
+++ src/components/SearchResultModal.js
@@ -0,0 +1,5 @@
+function SearchResultModal() {
+  return <div>Search Result Modal</div>;
+}
+
+export default SearchResultModal;
